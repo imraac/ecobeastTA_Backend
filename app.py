067a08b30639b4919bcec5b43eac399fa94dd961
imraac <agmraan@gmail.com>
2025-05-27 22:31:57 +0300

    done

--- app.py
+++ app.py
@@ -689,6 +689,8 @@ api.add_resource(Login, "/api/login")
 api.add_resource(VerifyToken, "/api/verify-token")
 
 
+import os
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    if os.environ.get("RAILWAY_ENVIRONMENT") is None:
+        app.run(debug=True, port=5000)
